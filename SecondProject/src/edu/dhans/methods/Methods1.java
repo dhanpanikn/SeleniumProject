3d569af578fa9c2eb87ffeee7f26dd92719d9c8f
dhans <dhans@DESKTOP-FVKV8M1>
2021-02-09 07:02:18 +0530

    my next commit

--- SecondProject/src/edu/dhans/methods/Methods1.java
+++ SecondProject/src/edu/dhans/methods/Methods1.java
@@ -3,8 +3,16 @@ package edu.dhans.methods;
 public class Methods1 {
 
 	public static void main(String[] args) {
-		String playerName = "Dhans";
-		int score = 2000;
+		String playerName = "DHans";
+		int score = 1000;
+
+		leaderBoardPosition(playerName, score);
+			
+		
+			}
+	public static void leaderBoardPosition(String playerName,  int score) {
+		
+		
 		if (score > 1000) {
 			System.out.println( playerName + " is holding position 1");
 	} else if (score < 1000 && score > 500) {
@@ -14,6 +22,6 @@ public class Methods1 {
     } else {
     		System.out.println( playerName + " is holding position 4");
     }
+		
 	}
-
 }
